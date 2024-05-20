b8aead20b98ac76d023eac410fedbcaa8ca2f2ac
MeisterYM <yannick_mart@yahoo.com>
2024-05-20 15:12:15 -0500

    Changed hello world and added Loops

--- Hello_World.go
+++ Hello_World.go
@@ -3,5 +3,7 @@ package main
 import "fmt"
 
 func main() {
+
+	//Say hello.
 	fmt.Print("Hello World, again!")
 }
---
+++ Loops.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Count to ten.
+	for i := 1; i < 11; i++ {
+		fmt.Print(i, "\n")
+	}
+}
